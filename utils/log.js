173a8fc4d70895627825487d32ed9d5f634b501f
yash <yashsharmaniet2172@gmail.com>
2024-08-05 14:11:15 +0530

    second commit

---
+++ utils/log.js
@@ -0,0 +1,12 @@
+export const displayFancyLogs = () => {
+  console.log(
+    "%c Yash Sharma Portfolio",
+    "background: #212121; color: #6b17e8; padding: 6px;"
+
+  );
+
+  console.log(
+    "%c Hope you like what you see :)",
+    "background: #212121; color: #6b17e8; padding: 6px;"
+  );
+};
